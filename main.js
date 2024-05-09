f478f2f29fa7be3e87b247e1540ed03199a95d56
idaliasaenz4 <162154055+idaliasaenz4@users.noreply.github.com>
2024-05-09 09:11:51 -0600

    version 1.0.3

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-var express = require ('express');
-var app = express();
+var express = require('express');
+var app=express();
+var server=require('http').Server(app);
+var io = require ('socket.io')(server);
 
-//como trabajaremos con socket, es recomendado usar el modulo HTTP 
-//para pasarle la app a express y manejar bien http
+/Usamo un middleware para usar elementos estaticos en la seccion publica de la aplicación/
+app.use(express.static('public'));
 
-var server = require('http').Server(app);
+app.get('/', function(req,res){
+    res.status(200).send("Hola mundo");
+});
 
-//aqui estara toda la funcionalidad de los sockets
-// se requiere la libreria socket.io
-//se pasa la variable Server que tiene la app express y HTTP
-
-var io = require('socket.io')(server);
-
-app.get('/',function(req, res){
-    res.status(200).send("Hola Mundo")
+/*De esta forma activamos socket para que este escuchando mandamos un mensaje de control por consola para saber que pasa
+y tenemos que hacer que el mensaje venga del navegador web mediante html y JS */ 
+io.on('connection', function(socket){
+    console.log('Alguien se ha conectado con socket')
+    socket.emit('message',{
+        id:1,
+        texto: "Hola soy un mensaje",
+        autor: "Idalia Saenz Martinez"
+    });
 });
 
 server.listen(3002, function(){
-    console.log("el servidor esat corriendo en http://localHost:3002");
+    console.log("El servidor esta corriendo en http://localhost:3002");
 });
